a09ee6d5b992c9a71bd8ab5bf3874a41f96145e3
MBTechEDev <marcos@mbtechedev.com>
2026-03-04 22:42:02 -0300

    feat: add `backendUrl` configuration to Medusa admin settings

--- medusa-config.ts
+++ medusa-config.ts
@@ -19,7 +19,8 @@ module.exports = defineConfig({
     }
   },
   admin: {
-    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000",
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true"
   },
   modules: [
     {
